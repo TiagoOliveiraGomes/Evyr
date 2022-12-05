b045a32e7b7a7fcc383041fbde0ca02e08786844
TiagoOliveiraGomes <tyagooGomes@gmail.com>
2022-12-04 23:49:56 -0300

    finish blackBox component

--- src/components/template/blackBox/blackBox.stories.tsx
+++ src/components/template/blackBox/blackBox.stories.tsx
@@ -1,17 +1,25 @@
 import { Meta, StoryObj } from '@storybook/react'
+import { FlagBrasil } from '../../../assets/imgs/flags/brasil'
 import { ConfirmButton } from '../../commons/confirmButton'
 import { Input } from '../../commons/input'
 import { BlackBox, BlackBoxProps } from './'
+import { CaretDown } from 'phosphor-react'
 
 export default {
     title: 'Components/TEMPLATE/BlackBox',
     component: BlackBox,
     args: {
         children: (
-            <div className='flex flex-col justify-center items-center gap-14 w-3'>
-                <Input placeholder='Ex: Joãozin' />
-                <ConfirmButton>Iniciar</ConfirmButton>
-            </div>
+        <div className='flex flex-col justify-center items-center gap-14'>
+          <div className='flex gap-5 items-center'>
+              <Input placeholder='Ex: Joãozin' />
+              <div className='flex items-center gap-2'>
+                <FlagBrasil />
+                <CaretDown width={25} height={25} color='white' />
+              </div>
+          </div>
+          <ConfirmButton>Iniciar</ConfirmButton>
+        </div>
         ) 
     }
 } as Meta<BlackBoxProps>
--- src/pages/start/index.tsx
+++ src/pages/start/index.tsx
@@ -1,19 +1,26 @@
 import React from 'react'
+import './styles.css'
+
 import { ConfirmButton } from '../../components/commons/confirmButton'
 import { Input } from '../../components/commons/input'
 import { BlackBox } from '../../components/template/blackBox'
-import './styles.css'
+import { FlagBrasil } from '../../assets/imgs/flags/brasil'
+import { CaretDown } from 'phosphor-react'
 
 export function StartPage() {
   return (
     <div className='StartPage-Container'>
         <BlackBox>
         <div className='flex flex-col justify-center items-center gap-14'>
-          <div>
+          <div className='flex gap-5 items-center'>
               <Input placeholder='Ex: Joãozin' />
+              <div className='flex items-center gap-2'>
+                <FlagBrasil />
+                <CaretDown width={25} height={25} color='white' />
+              </div>
           </div>
-              <ConfirmButton>Iniciar</ConfirmButton>
-            </div>
+          <ConfirmButton>Iniciar</ConfirmButton>
+        </div>
         </BlackBox>
     </div>
   )
