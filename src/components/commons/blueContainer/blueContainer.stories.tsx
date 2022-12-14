089fd89adfeacd49723f0c6a1a44617e2a45a2e3
TiagoOliveiraGomes <tyagooGomes@gmail.com>
2022-12-13 21:31:26 -0300

    Finishes blueComponent

--- src/components/commons/blueContainer/blueContainer.stories.tsx
+++ src/components/commons/blueContainer/blueContainer.stories.tsx
@@ -1,8 +1,12 @@
-import { BlueContainer } from ".";
+import { BlueContainer, BlueContainerProps } from ".";
+import { Meta, StoryObj } from '@storybook/react'
 
 export default {
     title: 'Components/COMMONS/BlueContainer',
-    component: BlueContainer
-}
+    component: BlueContainer,
+    args: {
+        selected: true
+    }
+} as Meta<BlueContainerProps>
 
-export const Default = {}
+export const Default:StoryObj<BlueContainerProps> = {}
--- src/components/commons/blueContainer/index.tsx
+++ src/components/commons/blueContainer/index.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import './styles.css'
 
-export function BlueContainer() {
+export interface BlueContainerProps {
+    selected: boolean,
+}
+export function BlueContainer(props: BlueContainerProps) {
+    const { selected } = props
   return (
     <div className='BlueContainer-Container'>
         <header>
@@ -9,8 +13,18 @@ export function BlueContainer() {
         </header>
         <main>
             <ul>
-                <li>
+                <li className={`${selected? 'bg-gradient-to-r from-transparent via-[#2f6ebab7]' : 'bg-transparent'}`}>
                     <h3>Cabeça</h3>
+                </li> 
+                {/* TODO: Colocarr um estado Select em todos os 'FALSE' */}
+                <li className={`${false? 'bg-gradient-to-r from-transparent via-[#2f6ebab7]' : 'bg-transparent'}`}>
+                    <h3>Corpo</h3>
+                </li>
+                <li className={`${false? 'bg-gradient-to-r from-transparent via-[#2f6ebab7]' : 'bg-transparent'}`}>
+                    <h3>Cauda</h3>
+                </li>
+                <li className={`${false? 'bg-gradient-to-r from-transparent via-[#2f6ebab7]' : 'bg-transparent'}`}>
+                    <h3>Arma</h3>
                 </li>
             </ul>
         </main>
