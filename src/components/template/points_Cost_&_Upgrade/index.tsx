01d4d28f80954575264102ea81c9ebb79bb78f9f
TiagoOliveiraGomes <tyagooGomes@gmail.com>
2022-12-19 16:26:44 -0300

    add list of upgrade and costs atributes

--- src/components/template/points_Cost_&_Upgrade/index.tsx
+++ src/components/template/points_Cost_&_Upgrade/index.tsx
@@ -1,18 +1,27 @@
-import React from 'react'
-import './styles.css'
-import { BiteIcon } from '../../../assets/imgs/icons/bite'
+import React from "react";
+import "./styles.css";
+import { BiteIcon } from "../../../assets/imgs/icons/bite";
 
 export function PointsOfCostAndUpgrade() {
   return (
-    <div className='PointsOfCostAndUpgrade-Container'>
-        <div>
+    <div className="PointsOfCostAndUpgrade-Container">
+        <div className="Atributes-points">
             <header>
                 <h1>Upgrade</h1>
-                <div className='icon'>
-                    <h3>+2</h3>
-                    <BiteIcon />
-                </div>
             </header>
+            <main>
+                <h3>+2</h3>
+                <BiteIcon />
+            </main>
+        </div>
+        <div className="Atributes-points">
+            <header>
+                <h1>Cost</h1>
+            </header>
+            <main>
+                <h3>+2</h3>
+                <BiteIcon />
+            </main>
         </div>
     </div>
   )
